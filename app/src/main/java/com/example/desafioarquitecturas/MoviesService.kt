bb8980ab9aa7ab9475f71e2bf5ccd4cc45c5149e
romulo.banares <romulo88@hotmail.com>
2023-10-04 17:51:16 +0200

    initial commit

---
+++ app/src/main/java/com/example/desafioarquitecturas/MoviesService.kt
@@ -0,0 +1,8 @@
+package com.example.desafioarquitecturas
+
+import retrofit2.http.GET
+
+interface MoviesService {
+    @GET("discover/movie?api_key=9fccbb77c2d087eb822c426d4d6ec411")
+    suspend fun getMovies(): MovieResult
+}
