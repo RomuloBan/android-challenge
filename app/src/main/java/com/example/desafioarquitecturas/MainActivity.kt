2439fa91d35cb18725a02633dafb78c1d3520a3c
romulo.banares <romulo88@hotmail.com>
2023-10-09 10:25:50 +0200

    fix imports

--- app/src/main/java/com/example/desafioarquitecturas/MainActivity.kt
+++ app/src/main/java/com/example/desafioarquitecturas/MainActivity.kt
@@ -4,12 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.desafioarquitecturas.ui.screens.home.Home
-import com.example.desafioarquitecturas.ui.theme.DesafioArquitecturasTheme
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
