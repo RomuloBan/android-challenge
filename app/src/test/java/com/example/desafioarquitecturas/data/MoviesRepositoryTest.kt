09405d8f331cb70c440fd4e699c60ab38429dbdc
romulo.banares <romulo88@hotmail.com>
2023-10-10 14:15:39 +0200

    add DB is empty movies are inserted

--- app/src/test/java/com/example/desafioarquitecturas/data/MoviesRepositoryTest.kt
+++ app/src/test/java/com/example/desafioarquitecturas/data/MoviesRepositoryTest.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.never
 import org.mockito.kotlin.verifyBlocking
 
 class MoviesRepositoryTest {
@@ -21,5 +22,40 @@ class MoviesRepositoryTest {
         runBlocking { repository.getMovies() }
 
         verifyBlocking(remoteDataSource) { getMovies() }
+    }
+
+    @Test
+    fun `When DB is not empty, server is not called`() {
+        val localDataSource = mock<LocalDataSource>() {
+            onBlocking { count() } doReturn 1
+        }
+        val remoteDataSource = mock<RemoteDataSource>()
+        val repository = MoviesRepository(localDataSource, remoteDataSource)
+
+        runBlocking { repository.getMovies() }
+
+        verifyBlocking(remoteDataSource, never()) { getMovies() }
+    }
+
+    @Test
+    fun `When DB is empty, movies are inserted`() {
+        val movieList = listOf(
+            Movie(1, "Title 1", "Overview 1", "Poster 1", false),
+            Movie(2, "Title 2", "Overview 2", "Poster 2", false),
+            Movie(3, "Title 3", "Overview 3", "Poster 3", false),
+        )
+        val localDataSource = mock<LocalDataSource>() {
+            onBlocking { count() } doReturn 0
+        }
+        val remoteDataSource = mock<RemoteDataSource>() {
+            onBlocking { getMovies() } doReturn movieList
+        }
+        val repository = MoviesRepository(localDataSource, remoteDataSource)
+
+        runBlocking { repository.getMovies() }
+
+        verifyBlocking(localDataSource) {
+            insertAll(movieList)
+        }
     }
 }
